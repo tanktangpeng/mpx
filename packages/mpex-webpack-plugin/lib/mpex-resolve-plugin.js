a67a9d335772921a9a3128dd5790229eeeba3f62
ctxrr <iamwaynehu@gmail.com>
2018-12-28 14:35:00 +0800

    [mpex-webpack-plugin] done

--- packages/mpex-webpack-plugin/lib/mpex-resolve-plugin.js
+++ packages/mpex-webpack-plugin/lib/mpex-resolve-plugin.js
@@ -1,32 +1,56 @@
-// const DEFAULT_DIR = {
-//   wx: 'wx',
-//   ali: 'ali',
-//   vue: 'vue'
-// }
+const path = require('path')
+const DEFAULT_DIR = {
+  wx: 'wx',
+  ali: 'ali',
+  vue: 'vue'
+}
 
-// module.exports = class MpexResolvePlugin {
-//   constructor (source, options, target) {
-//     this.source = source
-//     this.options = options
-//     this.target = target
-//   }
+module.exports = class MpexResolvePlugin {
+  constructor (source, options, target) {
+    this.source = source
+    this.options = options
+    this.target = target
+  }
 
-//   apply (resolver) {
-//     const target = resolver.ensureHook(this.target)
-//     resolver.getHook(this.source).tapAsync('MpexResolvePlugin', (request, resolveContext, callback) => {
-//       if (request.descriptionFileData.mpexRuntime) {
-//         const mpexRuntime = request.descriptionFileData.mpexRuntime
-//         const dirMaps = typeof mpexRuntime === 'object' ? mpexRuntime : DEFAULT_DIR
-//         const obj = Object.assign({}, request, {
-//           // request: './' + dirMaps[this.options.mode] + '/index.js'
-//           request: './wx/index.js',
-//           path: request.path + '/wx/index.js'
-//         })
-//         resolver.doResolve(target, obj, 'mpex runtime ' + this.options.mode, resolveContext, callback)
-//         // return callback()
-//       } else {
-//         return callback()
-//       }
-//     })
-//   }
-// }
+  apply (resolver) {
+    const target = resolver.ensureHook(this.target)
+    resolver.getHook(this.source).tapAsync('MpexResolvePlugin', (request, resolveContext, callback) => {
+      if (request.descriptionFileData.mpex) {
+        // 不转换相对路径的引用
+        if (request.request) return callback()
+
+        // 计算拼接路径
+        // 例如：import xxx from '@mpxjs/mpex-runtime/a/b/c' -> a/b/c
+        const fakeResourcePath = path.resolve(request.descriptionFileRoot, request.relativePath)
+        const dir = path.relative(request.descriptionFileRoot, fakeResourcePath)
+
+        // 不转换携带ignore参数的
+        let query = request.query.match(/(\?.+)/)
+        if (query) {
+          let queryStr = query[1].substr(1)
+          let parsed = queryStr.split('&')
+          let shouldIgnore = parsed.some(i => i.match(/ignore/))
+          if (shouldIgnore) return callback()
+        }
+
+        if (request.hasMpexResolved) return callback()
+
+        // start
+        const mpexRuntime = request.descriptionFileData.mpex
+        const dirMaps = typeof mpexRuntime === 'object' ? mpexRuntime : DEFAULT_DIR
+        const rootDir = dirMaps[this.options.mode]
+        const subPath = dir ? ('/' + dir) : ''
+        const obj = Object.assign({}, request, {
+          path: request.descriptionFileRoot, // always be the package root
+          request: './' + rootDir + subPath,
+          relativePath: '.', // always keep relativePath empty
+          hasMpexResolved: true
+        })
+
+        resolver.doResolve(target, obj, 'mpex runtime ' + this.options.mode, resolveContext, callback)
+      } else {
+        return callback()
+      }
+    })
+  }
+}
--- packages/mpex-webpack-plugin/lib/mpex-webpack-plugin.js
+++ packages/mpex-webpack-plugin/lib/mpex-webpack-plugin.js
@@ -5,15 +5,16 @@ class MpexWebpackPlugin {
   }
 
   apply (compiler) {
+    const MpexResolvePlugin = require('./mpex-resolve-plugin')
     compiler.hooks.normalModuleFactory.tap('MpexWebpackPlugin', (normalModuleFactory) => {
       normalModuleFactory.resolverFactory.hooks.resolveOptions
         .for('normal')
         .tap('MpexWebpackPlugin', (resolveOptions) => {
           if (resolveOptions) {
-            let mainFields = resolveOptions.mainFields || (resolveOptions.mainFields = [])
-            let newMainField = this.mode + 'Main'
-            if (!mainFields.includes(newMainField)) {
-              resolveOptions.mainFields.unshift(this.options.mode + 'Main')
+            let plugins = resolveOptions.plugins || (resolveOptions.plugins = [])
+            let has = plugins.some(i => i instanceof MpexResolvePlugin)
+            if (!has) {
+              plugins.push(new MpexResolvePlugin('before-described-relative', { mode: this.options.mode }, 'resolve'))
             }
           } else {
             resolveOptions = {}
